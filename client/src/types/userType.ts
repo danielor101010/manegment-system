d43dc5f132df3a1d09681a17c019290b92fcd721
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2024-03-10 18:34:57 +0200

    added  interface

---
+++ client/src/types/userType.ts
@@ -0,0 +1,13 @@
+
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    city: string;
+  }
+  
+  export default User;
+  

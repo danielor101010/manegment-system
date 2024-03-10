10534436ff97e416fd5df9b2cfc9efb6cf7d8a67
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2024-03-10 12:48:43 +0200

    update packages

--- Server/serverConnection/mongoConnection.ts
+++ Server/serverConnection/mongoConnection.ts
@@ -2,7 +2,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const uri = "mongodb+srv://danielor:daniel112@cluster0.viv6s3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,13 +12,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }

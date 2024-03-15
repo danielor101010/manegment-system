618f0cf0b5ce719972e6a8126b7f62c47df0f229
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2024-03-15 10:51:41 +0200

    getUserHooks finally work!

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from 'react-query';
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,8 +7,11 @@ import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+  <App />
+</QueryClientProvider>
 );
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -3,14 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import WorkerDashboard from "../pages/workerDashboard/WorkerDashboard";
-import AdminDahsboard from "../pages/adminDashboard/AdminDahsboard";
+import AdminDashboard from "../pages/adminDashboard/AdminDahsboard";
 
 function Router() {
   const router = createBrowserRouter([
     { path: "/login", element: <Login /> },
     { path: "/register", element: <Register /> },
     { path: "/workerDashboard", element: <WorkerDashboard /> },
-    { path: "/adminDahsboard", element: <AdminDahsboard /> }
+    { path: "/adminDashboard", element: <AdminDashboard /> }
   ]);
 
   return <RouterProvider router={router} />;
